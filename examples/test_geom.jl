f8443755dbbc03ab14bc02accf76d48d53bd1717
xzackli <xzackli@gmail.com>
2020-03-16 17:08:13 -0400

    doesn't seem to modify the array, uh oh

--- examples/test_geom.jl
+++ examples/test_geom.jl
@@ -4,28 +4,29 @@
 using LibSharp
 using Healpix
 ##
-# alm = make_triangular_alm_info(10, 10, 1)
+
 nside = 4
-lmax = 4
+lmax = 2
 npix = 12*nside*nside
 n_alm = Int64( ((lmax+1)*(lmax+2))/2 )
 geom_info = make_weighted_healpix_geom_info(nside, 1)
 alm_info = make_triangular_alm_info(lmax, lmax, 1)
 
 ##
-map_size(geom_info)
+# maps = Array{Cdouble,1}[rand(Cdouble, (npix))]
+# alms = Array{ComplexF64,1}[zeros(ComplexF64, (n_alm))]
 
-##
-maps = Array{Cdouble,1}[rand(Cdouble, (npix))]
-alms = Array{ComplexF64}[zeros(ComplexF64, (n_alm))]
-
+map0 = ones(Cdouble, (npix))
+refMap = [Ref(map0, 1)]
+alm0 = zeros(ComplexF64, (n_alm))
+refAlm = [Ref(alm0, 1)]
 ##
 LibSharp.sharp_execute(
     LibSharp.SHARP_MAP2ALM, 0, 
-    alms, 
-    maps, 
+    refAlm, 
+    refMap, 
     geom_info, alm_info, 
     LibSharp.SHARP_DP)
 ##
-print(alms)
+println(alm0)
 ##
