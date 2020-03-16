4ad5d430ab38b05dc5ea98acea8a500ee653b178
xzackli <xzackli@gmail.com>
2020-03-16 11:55:29 -0400

    add GeomInfo struct and finalizer

--- src/LibSharp.jl
+++ src/LibSharp.jl
@@ -4,6 +4,7 @@ using Libdl
 using libsharp2_jll
 
 export AlmInfo, make_alm_info, alm_index, alm_count
+export make_triangular_alm_info
 
 mutable struct AlmInfo
     ptr::Ptr{Cvoid}
@@ -11,7 +12,8 @@ mutable struct AlmInfo
     AlmInfo(ptr::Ptr{Cvoid}) = finalizer(destroy_alm_info, new(ptr))
 end
 
-function make_alm_info(lmax::Integer, mmax::Integer, stride::Integer, mstart::AbstractArray{T}) where T <: Integer
+function make_alm_info(lmax::Integer, mmax::Integer, stride::Integer, 
+                       mstart::AbstractArray{T}) where T <: Integer
     alm_info_ptr = Ref{Ptr{Cvoid}}()
     mstart_cint = [Cint(x) for x in mstart]
     ccall(
@@ -19,6 +21,27 @@ function make_alm_info(lmax::Integer, mmax::Integer, stride::Integer, mstart::Ab
         Cvoid,
         (Cint, Cint, Cint, Ref{Cint}, Ref{Ptr{Cvoid}}),
         lmax, mmax, stride, mstart_cint, alm_info_ptr,
+    )
+
+    AlmInfo(alm_info_ptr[])
+end
+
+"""
+Initialises an a_lm data structure according to the scheme 
+used by Healpix_cxx.
+ 
+### Returns
+ AlmInfo object
+"""
+function make_triangular_alm_info(lmax::Integer, mmax::Integer, 
+                                  stride::Integer)
+
+    alm_info_ptr = Ref{Ptr{Cvoid}}()
+    ccall(
+        (:sharp_make_triangular_alm_info, libsharp2),
+        Cvoid,
+        (Cint, Cint, Cint, Ref{Ptr{Cvoid}}),
+        lmax, mmax, stride, alm_info_ptr,
     )
 
     AlmInfo(alm_info_ptr[])
@@ -53,5 +76,29 @@ alm_count(alm::AlmInfo) = ccall(
     (Ptr{Cvoid},),
     alm.ptr,
 )
+
+
+mutable struct GeomInfo
+    ptr::Ptr{Cvoid}
+
+    GeomInfo(ptr::Ptr{Cvoid}) = finalizer(destroy_geom_info, new(ptr))
+end
+
+function destroy_geom_info(info::GeomInfo)
+    ptr = info.ptr
+
+    if ptr != C_NULL
+        info.ptr = C_NULL
+        ccall(
+            (:sharp_destroy_geom_info, libsharp2),
+            Cvoid,
+            (Ptr{Cvoid},),
+            ptr,
+        )
+    end
+end
+
+# ADD THIS FOR HEALPIX
+# sharp_make_weighted_healpix_geom_info(cs->n_eq,1,NULL,&geom_info);
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,10 @@ using Test
 
 @testset "LibSharp.jl" begin
     alm = make_alm_info(10, 10, 1, 0:10)
-    @assert alm_count(alm) == 66
+    @test alm_count(alm) == 66
+
+    triangular_alm = make_triangular_alm_info(10, 10, 1)
+    @test alm_count(triangular_alm) == 66
+
     println(alm_index(alm, 1, 1))
 end
